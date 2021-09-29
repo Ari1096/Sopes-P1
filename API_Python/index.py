aa9e05336811ccdf86dedec571414aa4f317883d
Ari1096 <34315100+Ari1096@users.noreply.github.com>
2021-09-28 18:45:15 -0600

    update api

--- API_Python/index.py
+++ API_Python/index.py
@@ -1,44 +1,44 @@
-# MODULOS
+# --------------------------------------------- MODULOS ---------------------------------------------
 # Para errores - pip install pylint
 
 # Se llama al Framework y se inicializa
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 # Módulo para mysql - pip install mysqlclient
-import MySQLdb
+import MySQLdb 
 
-# Módulo para MongoDB - pip install pymongo
-# import pymongo
-
-# Credenciales 
-# MySQL
-# DB_HOST = '34.135.208.19' 
-# DB_USER = 'root' 
-# DB_PASS = 'dbace2p1' 
-# DB_NAME = 'db2proyecto1' 
-
+# --------------------------------------------- FUNCIONES ---------------------------------------------
 # Conexión con MySQL
-def run_query(query=''): 
+def GetMySQL(query=''): 
     # Credenciales
+    DB_HOST = '35.238.36.76' 
+    DB_USER = 'root' 
+    DB_PASS = 'dbsopes1' 
+    DB_NAME = 'sopes1data'
     datos = [DB_HOST, DB_USER, DB_PASS, DB_NAME] 
     
-    conn = MySQLdb.connect(*datos) # Conectar a la base de datos 
-    cursor = conn.cursor()         # Crear un cursor 
-    cursor.execute(query)          # Ejecutar una consulta 
+    # Se conecta a la DB y se crea un cursor
+    conn = MySQLdb.connect(*datos)
+    cursor = conn.cursor()
 
+    # Se ejecuta una consulta
+    cursor.execute(query)
+
+    # Si es un select se traen todos los resultados, de lo contrario es una escritura de datos
     if query.upper().startswith('SELECT'): 
-        data = cursor.fetchall()   # Traer los resultados de un select 
+        data = cursor.fetchall()
     else: 
-        conn.commit()              # Hacer efectiva la escritura de datos 
+        conn.commit()
         data = None 
     
-    cursor.close()                 # Cerrar el cursor 
-    conn.close()                   # Cerrar la conexión 
+    # Se cierra el cursor y la conexión
+    cursor.close()
+    conn.close()
 
     return data
 
-# Conexión con MongoDB
-def getDatabase():
+# Conexión con MongoDB, aquí se utiliza el módulo MongoDB pip install pymongo - pip install dnspython
+def GetMongo():
     # Import inicial
     from pymongo import MongoClient
     import pymongo
@@ -50,46 +50,57 @@ def getDatabase():
     from pymongo import MongoClient
     client = MongoClient(CONNECTION_STRING)
 
-    # Create the database for our example (we will use the same database throughout the tutorial
+    # Create the database for our example
     return client['Clase7']
 
 # Se designa que este es el archivo principal, el que va a arrancar la aplicación
 # app va a ser la variable que permitirá crear las rutas del servidor, URL, etc
 app = Flask(__name__)
 
-# ------------------------- RUTAS -------------------------
+# --------------------------------------------- RUTAS ---------------------------------------------
 # Para crear una ruta primero se utiliza un decorador @
 @app.route('/')
 def home():
+    return 'Hola'
+@app.route('/iniciarCarga', methods=["POST"])
+def iniciarCarga():
+    # Se decodifica el JSON recibido, get_json() hace un parsing
+    jsonres = request.get_json()
+
+    # ----------------------------- MONGO -----------------------------
     # Get the database
-    dbname = getDatabase()
+    dbname = GetMongo()
 
     # Get the collection
     collection_name = dbname["data"]
 
-    # Dato a introducir
-    item_3 =  {
-    "nombre": "Sebastian",
-    "comentario": "Tweet:5",
-    "hashtags": [
-        "tweet5"
-    ],
-    "upvotes": 30,
-    "downvotes": 27
-    }
+    # Se realiza la inserción en la conexión correspondiente
+    collection_name.insert_one(jsonres)
 
-    collection_name.insert_one(item_3)
+    # ----------------------------- MYSQL -----------------------------
+    # Se prepara el primer query y se llama a la función de inserción de usuario y tweet
+    query = "CALL PROC2('{0}', '{1}', CURDATE(), {2}, {3});".format(jsonres["nombre"], jsonres["comentario"], jsonres["upvotes"], jsonres["downvotes"])
+    data1 = GetMySQL(query)
 
-    return 'Hola'
-@app.route('/iniciarCarga')
-def iniciarCarga():
-    return render_template('iniciarCarga.html')
+    # Se prepara el segundo query y se llama a la función de selección del último tweet
+    query2 = "SELECT Id_tweet FROM Tweet ORDER BY Id_tweet DESC LIMIT 1;"
+    idretornado = GetMySQL(query2)
+
+    # Se hará una inserción por cada dato de la lista en el json
+    for i in range (0, len(jsonres["hashtags"])):
+        # Se prepara el tercer query y se llama a la función de inserción de hashtag
+        query3 = "CALL PROC1({0}, '{1}');".format(idretornado[0][0], jsonres['hashtags'][i])
+        inhashtag = GetMySQL(query3)
+
+    return 'Carga completada'
 @app.route('/publicar')
 def publicar():
-    return render_template('about.html')
+    return 'Publicar'
+    # return render_template('publicar.html')
 @app.route('/finalizarCarga')
 def finalizarCarga():
-    return render_template('about.html')
+    return 'Finalizar Carga'
+    # return render_template('about.html')
 
 # ------------------------- MÉTODO PRINCIPAL -------------------------
 if(__name__ == '__main__'):
